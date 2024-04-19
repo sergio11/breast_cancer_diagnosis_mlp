246ed9a63b85812e23fa5e213ce8e016098cd753
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-04-19 21:26:15 +0200

    Implement several improvements

--- setup.py
+++ setup.py
@@ -1,26 +1,51 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mlp-breast-cancer-diagnosis',
-    version='1.0',
+    name='BreastCancerMLPModel',  
+    version='0.0.30',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'scikit-learn==0.24.2',
-        'numpy==1.21.2'
+        'scikit-learn==1.4.2',
+        'numpy==1.25.2'
     ],
-    author='DreamSoftware',
+    author='Sergio Sánchez Sánchez',
     author_email='dreamsoftware92@gmail.com',
     description='A package for breast cancer diagnosis using MLP classifier.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     url='https://github.com/sergio11/breast_cancer_diagnosis_mlp',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
+    python_requires='>=3.7, <4',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown'
 )
+
+"""
+BreastCancerMLPModel Setup
+
+This setup script configures the installation of the BreastCancerMLPModel package. BreastCancerMLPModel is a package for breast cancer diagnosis using MLP classifier.
+
+Project Details:
+- Name: BreastCancerMLPModel
+- Version: 0.0.30
+- Author: Sergio Sánchez Sánchez
+- Email: dreamsoftware92@gmail.com
+- Description: A package for breast cancer diagnosis using MLP classifier.
+- Repository: https://github.com/sergio11/breast_cancer_diagnosis_mlp
+
+Development Status: Beta
+
+License: MIT License
+
+Python Version Compatibility: >=3.7, <4
+
+For more details, please refer to the project's README.md file.
+"""
--- tests/test_mlp_model.py
+++ tests/test_mlp_model.py
@@ -1,23 +1,49 @@
 import unittest
-import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 from mlp_breast_cancer_diagnosis.mlp_model import BreastCancerMLPModel
 
 class TestMLPModel(unittest.TestCase):
     def setUp(self):
         self.model = BreastCancerMLPModel()
-        self.X_train = np.array([[1, 2], [3, 4]])
-        self.y_train = np.array([0, 1])
-        self.X_test = np.array([[5, 6], [7, 8]])
-        self.y_test = np.array([1, 0])
 
-    def test_train(self):
-        self.model.train(self.X_train, self.y_train)
+        # Load breast cancer dataset
+        breast_cancer = load_breast_cancer()
+        X = breast_cancer.data
+        y = breast_cancer.target
+
+        # Split dataset into train and test sets
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    def test_fit(self):
+        self.model.fit()
+
+        # Ensure that the model is trained successfully
         self.assertIsNotNone(self.model.model)
 
-    def test_evaluate(self):
-        self.model.train(self.X_train, self.y_train)
-        accuracy, _ = self.model.evaluate(self.X_test, self.y_test)
-        self.assertTrue(isinstance(accuracy, float))
+    def test_predict(self):
+        # Train the model
+        self.model.fit()
+
+        # Create a sample input data
+        data = ("mean_radius: 13.08, mean_texture: 15.71, mean_perimeter: 85.63, mean_area: 520, "
+                "mean_smoothness: 0.1075, mean_compactness: 0.127, mean_concavity: 0.04568, "
+                "mean_concave_points: 0.0311, mean_symmetry: 0.1967, mean_fractal_dimension: 0.06811, "
+                "se_radius: 0.1852, se_texture: 0.7477, se_perimeter: 1.383, se_area: 14.67, "
+                "se_smoothness: 0.004097, se_compactness: 0.01898, se_concavity: 0.01698, "
+                "se_concave_points: 0.00649, se_symmetry: 0.01678, se_fractal_dimension: 0.002425, "
+                "worst_radius: 14.5, worst_texture: 20.49, worst_perimeter: 96.09, worst_area: 630.5, "
+                "worst_smoothness: 0.1312, worst_compactness: 0.2776, worst_concavity: 0.189, "
+                "worst_concave_points: 0.07283, worst_symmetry: 0.3184, worst_fractal_dimension: 0.08183")
+
+        # Make a prediction
+        predicted_label, probability = self.model.predict(data)
+
+        # Ensure that the predicted label is either "Maligno" or "Benigno"
+        self.assertIn(predicted_label, ["Maligno", "Benigno"])
+
+        # Ensure that the probability is a float
+        self.assertTrue(isinstance(probability, float))
 
 if __name__ == '__main__':
     unittest.main()
